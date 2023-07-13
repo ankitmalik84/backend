4b3648e3555978ec1583e38fbbde088204465a80
Ankit Malik <68542020+ankitmalik84@users.noreply.github.com>
2023-07-13 21:08:36 +0530

    modified:   index.js

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.use("/api/v1/payment", paymentRoutes);
 app.get("/", (req, res) => {
   return res.json({
     success: true,
-    message: "Your server is up and running....",
+    message: "Your server is up and running..........................👻",
   });
 });
 
